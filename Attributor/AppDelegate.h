656e0175bd27c9312ed6bc1dc72eb13f352b384f
ricardo <ricardo.batista07@gmail.com>
2015-01-05 17:06:44 -0400

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Ricardo Batista on 1/5/15.
+//  Copyright (c) 2015 XdroneStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Ricardo Batista on 1/5/15.
+//  Copyright (c) 2015 XdroneStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
